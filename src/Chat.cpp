d410c381001a00e524ba5d4fedab453a4d18c376
KornilovIgor <mail@kote.dev>
2024-04-21 21:44:36 +0300

    minifix

--- src/Chat.cpp
+++ src/Chat.cpp
@@ -335,7 +335,7 @@ bool Chat::isValidLogin(const std::string& login ) const
 
 	for (char ch : login)
 	{
-		if (ch >= 48 && ch <= 57 || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+		if ((ch >= 48 && ch <= 57) || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
 		{
 			continue;
 		}
